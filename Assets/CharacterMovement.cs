cfcf42a4be5318deedce5a573b4759986a397f58
CSullivan2023 <145032366+CSullivan2023@users.noreply.github.com>
2023-11-10 11:55:43 +0000

    removing movement bug

--- Assets/CharacterMovement.cs
+++ Assets/CharacterMovement.cs
@@ -16,29 +16,17 @@ public class CharacterMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("vertical");
+
+        rb.velocity = new Vector3(horizontalInput * 5f, rb.velocity.y, verticalInput * 5f);
+
         if (Input.GetButtonDown("Jump"))
         {
             rb.velocity = new Vector3(rb.velocity.x, 5f, rb.velocity.z);
         }
 
-        if (Input.GetKey("up"))
-        {
-            rb.velocity = new Vector3(0, 0, 5f);
-        }
-
-        if (Input.GetKey("right"))
-        {
-            rb.velocity = new Vector3(5f, 0, 0);
-        }
-        if (Input.GetKey("down"))
-        {
-            rb.velocity = new Vector3(0, 0, -5f);
-        }
-
-        if (Input.GetKey("left"))
-        {
-            rb.velocity = new Vector3(-5f, 0, 0);
-        }
+       
     }
 
 }
